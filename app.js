be330ec5de7a654634292c5dc33a05dfc58275f9
RuannaC-137 <ruanlons@gmail.com>
2023-01-20 20:16:14 -0300

    [FEAT]: tratando os erros

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
+var createError = require('http-errors');
+
 const app = express()
 
 app.use(cors())
@@ -8,8 +10,20 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 require("./models")
 require("./routers")(app)
-app.listen(process.env.PORT, err => {
-    err 
-    ? console.log(err) 
-    : console.log(`servidor rodando na porta ${process.env.PORT}`)
-})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+  
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json({message: res.locals.message});
+});
+  
+module.exports = app;
