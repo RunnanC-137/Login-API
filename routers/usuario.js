71bd08418e41ed5b88a221a3fbfbb07dca9b0c96
RuannaC-137 <ruanlons@gmail.com>
2023-03-09 14:46:52 -0300

    [FEAT]: add a rota de trocar senha

--- routers/usuario.js
+++ routers/usuario.js
@@ -6,6 +6,7 @@ const permition = require("../middlewares/permitions")
 router.get("/", permition.administrador, usuarioController.list)
 router.put("/:id", permition.administrador, usuarioController.update.adm)
 router.put("/", usuarioController.update.normal)
+router.put("/senha", usuarioController.update.password)
 router.delete("/:id", permition.administrador, usuarioController.delete.adm)
 router.delete("/", usuarioController.delete.normal)
 
